605e42f024be0d373a5f5fa7dd757949e5ca26b4
fnwede <fnwede@cogility.com>
2015-11-09 13:39:45 -0800

    Adding the tar for the latest CEAUI build.

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -14,3 +14,18 @@ end
 service "nginx" do
 	action :start
 end
+
+directory '/opt/cogility' do
+	owner 'ec2-user'
+	group 'ec2-user'
+	mode '0755'
+	action :create
+end
+
+remote_file '/opt/cogility/ceaui-static.tar.gz' do
+	source 'https://s3-us-west-1.amazonaws.com/cogility-builds/ceaui/ceaui-static.latest.tar.gz'
+	owner 'ec2-user'
+	group 'ec2-user'
+	mode '0755'
+	action :create	
+end
