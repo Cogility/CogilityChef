b75a4d44e4a18d3b1e23310b98e4b81faefbafee
Farye Nwede <fnwede@cogility.com>
2015-12-11 15:40:26 -0800

    Adding NGINX cookbook.

---
+++ cognode-nginx/metadata.rb
@@ -0,0 +1,9 @@
+name             'cognode-nginx'
+maintainer       'Farye Nwede'
+maintainer_email 'fnwede@cogility.com'
+license          'All rights reserved'
+description      'Installs/Configures cognode-nginx'
+long_description 'Installs/Configures cognode-nginx'
+version          '0.1.0'
+
+recipe 'cognode-nginx', 'Installs/Configures cognode-nginx'
---
+++ cognode-nginx/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: cognode-nginx
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+service "nginx" do
+	action :stop
+end 
+
+file "/etc/nginx/nginx.conf" do
+	action :delete
+end
+
+template "/etc/nginx/nginx.conf" do 
+	source "nginx.erb"
+	owner "root"
+	group "root"
+	mode "0755"
+end
+
+service "nginx" do
+	action :start
+end
+
+directory '/opt/cogility' do
+	owner 'ec2-user'
+	group 'ec2-user'
+	mode '0755'
+	action :create
+end
+
+remote_file '/opt/cogility/ceaui-static.tar.gz' do
+	source 'https://s3-us-west-1.amazonaws.com/cogility-builds/ceaui/ceaui-static.latest.tar.gz'
+	owner 'ec2-user'
+	group 'ec2-user'
+	mode '0755'
+	action :create	
+end
