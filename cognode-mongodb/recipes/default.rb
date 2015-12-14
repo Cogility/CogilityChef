455d277e5adebd16505a52d661c3f42e1514a591
fnwede <fnwede@cogility.com>
2015-12-14 10:31:05 -0800

    Changing method of installing rpm.

--- cognode-mongodb/recipes/default.rb
+++ cognode-mongodb/recipes/default.rb
@@ -8,7 +8,7 @@
 #
 ['mongodb-enterprise-server-3.0.7-1.el6.x86_64.rpm', 'mongodb-enterprise-shell-3.0.7-1.el6.x86_64.rpm', 'mongodb-enterprise-tools-3.0.7-1.el6.x86_64.rpm'].each do |p| 
 # start from an array of packages, could be an attributes like node['my_namespace']['packages']
-	package p do # no need to do interpolation here, we just need the name
+	rpm_package p do # no need to do interpolation here, we just need the name
 		source "/tmp/#{p}" # Here we have to concatenate path and name
 		action :nothing # do nothing, just define the resource
 	end
