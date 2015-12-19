8f65f88169e318288ac4d33e822219aeec0cd9ed
fnwede <fnwede@cogility.com>
2015-12-18 16:36:59 -0800

    Changing attribute arrays.

--- cognode-nodejs/attributes/default.rb
+++ cognode-nodejs/attributes/default.rb
@@ -6,13 +6,13 @@
 # Copyright Cogility Software 2015
 #
 
-default[:cognode-nodejs][:cogility_directory] = '/opt/cogility'
-default[:cognode-nodejs][:user] = 'ec2-user'
-default[:cognode-nodejs][:group] = 'ec2-user'
-default[:cognode-nodejs][:root_group] = 'root'
+default[:cognode_nodejs][:cogility_directory] = '/opt/cogility'
+default[:cognode_nodejs][:user] = 'ec2-user'
+default[:cognode_nodejs][:group] = 'ec2-user'
+default[:cognode_nodejs][:root_group] = 'root'
 
-default[:cognode-nodejs][:cogilitynode_directory] = '/opt/cogility/CogilityNode'
-default[:cognode-nodejs][:engagements_directory] = '/opt/cogility/Engagements'
+default[:cognode_nodejs][:cogilitynode_directory] = '/opt/cogility/CogilityNode'
+default[:cognode_nodejs][:engagements_directory] = '/opt/cogility/Engagements'
 
-default[:cognode-nodejs][:java_install_dir] = '/opt'
-default[:cognode-nodejs][:java_home] = '/opt/java'
+default[:cognode_nodejs][:java_install_dir] = '/opt'
+default[:cognode_nodejs][:java_home] = '/opt/java'
