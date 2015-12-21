c8a9033d164a691e6f41e5490b6e2fd10223298d
fnwede <fnwede@cogility.com>
2015-12-21 09:54:09 -0800

    Updating Node.JS cookbook with correct attribute names.

--- cognode-nodejs/recipes/cogility.rb
+++ cognode-nodejs/recipes/cogility.rb
@@ -1,7 +1,7 @@
 # Recipe for cogility related stuff
-directory node['cognode-nodejs']['cogility_directory] do
-	owner node['cognode-nodejs']['user']
-	group node['cognode-nodejs']['group']
+directory node['cognode_nodejs']['cogility_directory] do
+	owner node['cognode_nodejs']['user']
+	group node['cognode_nodejs']['group']
 	mode '0755'
 	action :create
 end
--- cognode-nodejs/recipes/install.rb
+++ cognode-nodejs/recipes/install.rb
@@ -8,22 +8,22 @@
 #
 
 execute 'get_cognode_repo' do
-	cwd node['cognode-nodejs']['cogility_directory']
+	cwd node['cognode_nodejs']['cogility_directory']
 	command 'git clone https://7e0e7ee0251607393b9cd83e338157cd382f0259:x-oauth-basic@github.com/Cogility/CogilityNode.git'
-	creates node['cognode-nodejs']['cogilitynode_directory']
+	creates node['cognode_nodejs']['cogilitynode_directory']
 end
 
-directory node['cognode-nodejs']['engagements_directory'] do
-	owner node['cognode-nodejs']['user']
-	group node['cognode-nodejs']['group']
+directory node['cognode_nodejs']['engagements_directory'] do
+	owner node['cognode_nodejs']['user']
+	group node['cognode_nodejs']['group']
 	mode '0755'
 	action :create
 end
 
 execute 'install_java_8' do
-	cwd node['cognode-nodejs']['java_install_dir']
+	cwd node['cognode_nodejs']['java_install_dir']
 	command 'wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.tar.gz" && tar xzf jdk-8u60-linux-x64.tar.gz && ln -s jdk-8u60-linux-x64 java'
-	creates node['cognode-nodejs']['java_home']
+	creates node['cognode_nodejs']['java_home']
 end
 
 cookbook_file "/tmp/java_variables.conf" do
@@ -53,4 +53,6 @@ end
 
 execute 'install_nodemon' do 
 	cwd '/'
-	command '
+	command 'npm install nodemon -g' 
+	user 'root'
+end
