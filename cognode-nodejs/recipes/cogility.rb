151714286513fc476a52b7a143c6dd1dc1bf14c4
fnwede <fnwede@cogility.com>
2015-12-21 10:02:48 -0800

    Missing quotation.

--- cognode-nodejs/recipes/cogility.rb
+++ cognode-nodejs/recipes/cogility.rb
@@ -1,5 +1,5 @@
 # Recipe for cogility related stuff
-directory node['cognode_nodejs']['cogility_directory] do
+directory node['cognode_nodejs']['cogility_directory'] do
 	owner node['cognode_nodejs']['user']
 	group node['cognode_nodejs']['group']
 	mode '0755'
