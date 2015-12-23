c06f822d08b66dda8438caff99374f57a8455fb0
fnwede <fnwede@cogility.com>
2015-12-23 10:12:10 -0800

    I guess environment variables are not allowed in OpsWork

--- cognode-nodejs/recipes/compile.rb
+++ cognode-nodejs/recipes/compile.rb
@@ -1,19 +1,3 @@
-env 'COGMODHOME' do 
-	value '/opt/cogility/CogilityNode/FrontEnd'
-end 
-
-env 'COGMGRHOME' do 
-	value '/opt/cogility/CogilityNode/Runtime'
-end
-
-env 'COGPRJHOME' do
-	value '/opt/cogility/Engagements'
-end
-
-env 'COGJDKHOME' do 
-	value '/opt/java'
-end
-
 execute 'compile_cogility_node' do
         cwd '/opt/cogility/CogilityNode/Common'
         command './compile-all.pl master=yes'
