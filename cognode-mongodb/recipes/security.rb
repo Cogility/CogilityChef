c4c89633a8a7a9c9189b5d47989d439367d58522
fnwede <fnwede@cogility.com>
2015-12-29 14:25:16 -0800

    Handling semanage error.

--- cognode-mongodb/recipes/security.rb
+++ cognode-mongodb/recipes/security.rb
@@ -1,5 +1,5 @@
 execute 'configure_selinux' do 
-	command 'semanage port -a -t mongod_port_t -p tcp 27017'
+	command 'if [ -z "$(semanage port --list | grep 27017)" ]; then semanage port -a -t mongod_port_t -p tcp 27017; fi'
 	user 'root'
 	group 'root'
 	action :run
